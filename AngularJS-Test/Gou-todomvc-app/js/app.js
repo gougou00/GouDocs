7e3821594e2b9c0cbd06a8be22b65c87a65a04ad
GouGou00 <jerrychang4@163.com>
2017-01-17 22:57:43 +0800

    删除功能

--- AngularJS-Test/Gou-todomvc-app/js/app.js
+++ AngularJS-Test/Gou-todomvc-app/js/app.js
@@ -6,6 +6,17 @@
 	var demoApp = angular.module('MyToDoMvc', []);
 	// 创建控制器
 	demoApp.controller('MainController', ['$scope', function ($scope) {
+		function getId () {
+			var id = Math.random();
+			for (var i = 0; i < $scope.todos.length; i++) {
+				if ($scope.todos[i].id === id) {
+					id = getId();
+					break;
+				}
+			}
+			return id;
+		}
+		
 		// 文本框模型
 		$scope.text = '';
 		// 任务列表
@@ -18,12 +29,22 @@
 		$scope.add = function () {
 			$scope.todos.push({
 				// auto
-				id: $scope.todos.length+1,
+				id: getId(),
 				text: $scope.text,
 				completed: false
 			});
 			$scope.text = '';
 		};
+		// 删除功能
+		$scope.remove = function (id) {
+			for (var i = 0; i < $scope.todos.length; i++) {
+				if ($scope.todos[i].id === id) {
+					$scope.todos.splice(i, 1);
+					break;
+				}
+			}
+		};
+
 	}]);
 
 })(window);
