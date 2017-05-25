a2dd60cec5cc041b3aa7675a70831587db64e4b7
gougou00 <jerrychang4@163.com>
2017-05-25 23:37:13 +0800

    add vue project

---
+++ Vue-Test/my-second-vue-project/src/components/componentA.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ msgfromfather }}</h2>
+    <button v-on:click="onClickMe">open mouse~</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        msg: 'hello from component a'
+      }
+    },
+    props: ['msgfromfather'],
+    events: {
+      'onAddnew': function (items) {
+        console.log(items)
+      }
+    },
+    methods: {
+      onClickMe: function () {
+        // console.log(this.msgfromfather)
+        // this.$emit('child-tell-me', this.msg)
+        this.$dispatch('child-tell-me', this.msg)
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+  h1 {
+    color: #42b983;
+  }
+</style>
